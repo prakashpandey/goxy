a50fbff4bad51ad9d43512738eacf32a594dba0c
Prakash Pandey <pcpandey@mail.com>
2018-10-17 13:00:15 +0530

    Adding go build and go run script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#################################
+# @author Prakash Pandey
+# @email prakashpandeyy@yahoo.com
+# @create date 2018-10-17 12:57:54
+# @modify date 2018-10-17 12:57:54
+# @desc [description]
+##################################
+
+#!/bin/bash
+
+go build
