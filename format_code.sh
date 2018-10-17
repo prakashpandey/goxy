23054f51bdf55a720a790947b9fd1ce974690287
Prakash Pandey <pcpandey@mail.com>
2018-10-18 05:00:00 +0530

    Added script to format your code

---
+++ format_code.sh
@@ -0,0 +1,10 @@
+#################################
+# @author Prakash Pandey
+# @email prakashpandeyy@yahoo.com
+# @create date 2018-10-17 4:10:54
+# @modify date 2018-10-17 4:10:54
+# @desc Format go code files
+##################################
+
+#!/bin/bash
+go fmt *.go
