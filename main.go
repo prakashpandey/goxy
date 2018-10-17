a8b1b2568a378e679d784d9a42434f8d658f2eb3
Prakash Pandey <pcpandey@mail.com>
2018-10-17 19:13:59 +0530

    Implimented proxy for http protocol

--- main.go
+++ main.go
@@ -8,27 +8,59 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-
+	log.Println("__INIT__ rootHandler")
+	if r.Method == http.MethodConnect {
+		handleHTTPS(w, r)
+	} else {
+		handleHTTP(w, r)
+	}
+	log.Println("__DONE__ rootHandler")
 }
 
-func handleHttp(w http.ResponseWriter, r *http.Request) {
+func handleHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("__INIT__ handleHTTP")
+	req, err := http.NewRequest(r.Method, r.RequestURI, r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// add headers to the request
+	for k, v := range r.Header {
+		req.Header.Set(k, v[0])
+	}
+	// server the target request
+	httpClient := &http.Client{}
+	resp, err := httpClient.Do(req)
+	r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 
+	// write the response back to the http connection
+	for k, v := range resp.Header {
+		w.Header().Set(k, v[0])
+	}
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+	resp.Body.Close()
+	log.Println("__DONE__ handleHTTP")
 }
 
-func handleHttps(w http.ResponseWriter, r *http.Request) {
-
+func handleHTTPS(w http.ResponseWriter, r *http.Request) {
+	log.Println("__INIT__ handleHTTPS")
+	log.Println("__DONE__ handleHTTPS")
 }
 
 func main() {
 	// get configuration such as hostname, port, timeout etc
 	config := getProxyConfig()
-	fmt.Printf("Running goxy at [hostName: %s, port: %d, timeout: %d]\n", config.hostName, config.port, config.timeout)
+	log.Printf("Running goxy at [hostName: %s, port: %d, timeout: %d]\n", config.hostName, config.port, config.timeout)
 
 	http.HandleFunc("/", rootHandler)
 	http.ListenAndServe(config.hostName+":"+strconv.Itoa(config.port), nil)
